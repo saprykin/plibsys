1995a0103c85f309a5c312cc2e706993839d3fc9
Alexander Saprykin <xelfium@gmail.com>
2013-06-19 11:11:49 +0400

    cmake: Detect MSVC-compatible compile flags on win32-gcc platform

--- platforms/win32-gcc/platform.cmake
+++ platforms/win32-gcc/platform.cmake
@@ -1,6 +1,20 @@
 set (PLIB_THREAD_MODEL win)
 set (PLIB_IPC_MODEL win)
 
+# Ensure MSVC-compatible struct packing convention is used
+# What flag to depends on gcc version: gcc3 uses '-mms-bitfields', while
+# gcc2 uses '-fnative-struct'.
+message (STATUS "Checking for MSVC-compatible code compilation flags")
+exec_program (gcc ARGS --version OUTPUT_VARIABLE CMAKE_C_COMPILER_VERSION)
+
+if (CMAKE_C_COMPILER_VERSION MATCHES ".*2\\.[0-9].*")
+	set (PLIB_PLATFORM_CFLAGS "-fnative-struct")
+	message (STATUS "Checking for MSVC-compatible code compile flags -fnative-struct")
+else()
+	set (PLIB_PLATFORM_CFLAGS "-mms-bitfields")
+	message (STATUS "Checking for MSVC-compatible code compile flags -mms-bitfields")
+endif()
+
 set (PLIB_PLATFORM_DEFINES
 	 -DHAVE_CONFIG_H
 )
