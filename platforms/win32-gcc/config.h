3417c9f6615ab74a50b866e0d6fe2f4c2eb1347d
Alexander Saprykin <xelfium@gmail.com>
2013-06-19 10:34:42 +0400

    cmake: Add win32-gcc platform (MinGW32)

---
+++ platforms/win32-gcc/config.h
@@ -0,0 +1,137 @@
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* define if asm blocks can use numeric local labels */
+/* #undef ASM_NUMERIC_LABELS */
+
+/* Define to 1 if you have the <arpa/inet.h> header file. */
+/* #undef HAVE_ARPA_INET_H */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+/* #undef HAVE_DLFCN_H */
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the <float.h> header file. */
+#define HAVE_FLOAT_H 1
+
+/* define to support printing 64-bit integers with format I64 */
+/* #undef HAVE_INT64_AND_I64 */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* define if system printf can print long long */
+#define HAVE_LONG_LONG_FORMAT 1
+
+/* Define to 1 if you have the `memmove' function. */
+#define HAVE_MEMMOVE 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `nanosleep' function. */
+/* #undef HAVE_NANOSLEEP */
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+/* #undef HAVE_NETINET_IN_H */
+
+/* Define to 1 if you have the `poll' function. */
+/* #undef HAVE_POLL */
+
+/* Define to 1 if you have the <sched.h> header file. */
+#define HAVE_SCHED_H 1
+
+/* Define to 1 if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H 1
+
+/* Define to 1 if the system has the type `socklen_t'. */
+/* #undef HAVE_SOCKLEN_T */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if the system has the type `struct sockaddr_storage'. */
+/* #undef HAVE_STRUCT_SOCKADDR_STORAGE */
+
+/* Define to 1 if you have the <sys/poll.h> header file. */
+/* #undef HAVE_SYS_POLL_H */
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+/* #undef HAVE_SYS_SOCKET_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <values.h> header file. */
+#define HAVE_VALUES_H 1
+
+/* Byte contents of pmutex */
+/* #undef PLIB_BYTE_CONTENTS_PMUTEX */
+
+/* The size of pmutex, as computed by sizeof. */
+/* #undef PLIB_SIZEOF_PMUTEX */
+
+/* The size of `char', as computed by sizeof. */
+#define SIZEOF_CHAR 1
+
+/* The size of `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of `long', as computed by sizeof. */
+#define SIZEOF_LONG 4
+
+/* The size of `long long', as computed by sizeof. */
+#define SIZEOF_LONG_LONG 8
+
+/* The size of `short', as computed by sizeof. */
+#define SIZEOF_SHORT 2
+
+/* The size of `size_t', as computed by sizeof. */
+#define SIZEOF_SIZE_T 4
+
+/* The size of `void *', as computed by sizeof. */
+#define SIZEOF_VOID_P 4
+
+/* The size of `__int64', as computed by sizeof. */
+#define SIZEOF___INT64 8
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
---
+++ platforms/win32-gcc/plibconfig.h
@@ -0,0 +1,106 @@
+#ifndef __PLIBCONFIG_H__
+#define __PLIBCONFIG_H__
+
+#include <pmacros.h>
+#include <limits.h>
+#include <float.h>
+
+P_BEGIN_DECLS
+
+#define P_MINFLOAT	FLT_MIN
+#define P_MAXFLOAT	FLT_MAX
+#define P_MINDOUBLE	DBL_MIN
+#define P_MAXDOUBLE	DBL_MAX
+#define P_MINSHORT	SHRT_MIN
+#define P_MAXSHORT	SHRT_MAX
+#define P_MAXUSHORT	USHRT_MAX
+#define P_MININT	INT_MIN
+#define P_MAXINT	INT_MAX
+#define P_MAXUINT	UINT_MAX
+#define P_MINLONG	LONG_MIN
+#define P_MAXLONG	LONG_MAX
+#define P_MAXULONG	ULONG_MAX
+
+typedef signed char pint8;
+typedef unsigned char puint8;
+typedef signed short	pint16;
+typedef unsigned short puint16;
+#define PINT16_MODIFIER		
+#define PINT16_FORMAT		"hi"
+#define PUINT16_FORMAT		"hu"
+
+typedef signed int pint32;
+typedef unsigned int puint32;
+#define PINT32_MODIFIER		""
+#define PINT32_FORMAT		"i"
+#define PUINT32_FORMAT		"u"
+typedef signed long long pint64;
+typedef unsigned long long puint64;
+#define PINT64_MODIFIER		"ll"
+#define PINT64_FORMAT		"lli"
+#define PUINT64_FORMAT		"llu"
+typedef signed int pssize;
+typedef unsigned int psize;
+#define PSIZE_MODIFIER		""
+#define PSSIZE_FORMAT		"i"
+#define PSIZE_FORMAT		"u"
+
+typedef pint64 poffset;
+#define POFFSET_MODIFIER	PINT64_MODIFIER
+#define POFFSET_FORMAT		PINT64_FORMAT
+
+#define P_MAXSIZE		P_MAXUINT
+#define P_MINSSIZE		P_MININT
+#define P_MAXSSIZE		P_MAXINT
+
+#define PLIB_SIZEOF_VOID_P	4
+#define PLIB_SIZEOF_LONG	4
+#define PLIB_SIZEOF_SIZE_T	4
+
+#define PPOINTER_TO_INT(p)	((pint)   (p))
+#define PPOINTER_TO_UINT(p)	((puint)  (p))
+
+#define PINT_TO_POINTER(i)	((ppointer)  (i))
+#define PUINT_TO_POINTER(u)	((ppointer)  (u))
+
+typedef signed int pintptr;
+typedef unsigned int puintptr;
+
+#define PINTPTR_MODIFIER      ""
+#define PINTPTR_FORMAT        "i"
+#define PUINTPTR_FORMAT       "u"
+
+
+typedef struct _PMutex* PStaticMutex;
+#define P_STATIC_MUTEX_INIT		NULL
+extern P_LIB_API PStaticMutex p_static_mutex_get_mutex_impl (PStaticMutex *mutex);
+#define p_static_mutex_get_mutex(mutex)	(p_static_mutex_get_mutex_impl (mutex))
+#define PINT16_TO_LE(val)	((pint16) (val))
+#define PUINT16_TO_LE(val)	((puint16) (val))
+#define PINT16_TO_BE(val)	((pint16) PUINT16_SWAP_BYTES (val))
+#define PUINT16_TO_BE(val)	(PUINT16_SWAP_BYTES (val))
+#define PINT32_TO_LE(val)	((pint32) (val))
+#define PUINT32_TO_LE(val)	((puint32) (val))
+#define PINT32_TO_BE(val)	((pint32) PUINT32_SWAP_BYTES (val))
+#define PUINT32_TO_BE(val)	(PUINT32_SWAP_BYTES (val))
+#define PINT64_TO_LE(val)	((pint64) (val))
+#define PUINT64_TO_LE(val)	((puint64) (val))
+#define PINT64_TO_BE(val)	((pint64) PUINT64_SWAP_BYTES (val))
+#define PUINT64_TO_BE(val)	(PUINT64_SWAP_BYTES (val))
+#define PLONG_TO_LE(val)	((plong) PINT32_TO_LE (val))
+#define PULONG_TO_LE(val)	((pulong) PUINT32_TO_LE (val))
+#define PLONG_TO_BE(val)	((plong) PINT32_TO_BE (val))
+#define PULONG_TO_BE(val)	((pulong) PUINT32_TO_BE (val))
+#define PINT_TO_LE(val)		((pint) PINT32_TO_LE (val))
+#define PUINT_TO_LE(val)	((puint) PUINT32_TO_LE (val))
+#define PINT_TO_BE(val)		((pint) PINT32_TO_BE (val))
+#define PUINT_TO_BE(val)	((puint) PUINT32_TO_BE (val))
+#define PSIZE_TO_LE(val)	((psize) PUINT32_TO_LE (val))
+#define PSSIZE_TO_LE(val)	((pssize) PINT32_TO_LE (val))
+#define PSIZE_TO_BE(val)	((psize) PUINT32_TO_BE (val))
+#define PSSIZE_TO_BE(val)	((pssize) PINT32_TO_BE (val))
+#define P_BYTE_ORDER		P_LITTLE_ENDIAN
+
+P_END_DECLS
+
+#endif /* __PLIBCONFIG_H__ */
