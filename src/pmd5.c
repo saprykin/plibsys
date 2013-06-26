8c1ce57a6c6fa2b401fbe50968113d4f882c7b44
Alexander Saprykin <xelfium@gmail.com>
2013-06-26 20:10:34 +0400

    Update comments in PHashMD5 (copyright + TODOs)

--- src/pmd5.c
+++ src/pmd5.c
@@ -1,6 +1,5 @@
 /* 
- * 27.10.2010
- * Copyright (C) 2010 Alexander Saprykin <xelfium@gmail.com>
+ * Copyright (C) 2010-2013 Alexander Saprykin <xelfium@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +17,6 @@
  */
 
 /* TODO: error reporting */
-/* TODO: convert to hex string */
 
 #include "pmem.h"
 #include "pmd5.h"
