3ba64899b22fbeab44d984cf298d94f80d3e8874
Alexander Saprykin <xelfium@gmail.com>
2013-06-26 20:29:59 +0400

    tests: Add yet another one test case for MD5 hash

--- tests/phash_test.cpp
+++ tests/phash_test.cpp
@@ -22,11 +22,20 @@ BOOST_AUTO_TEST_CASE (md5_test)
 	p_crypto_hash_update (md5_hash, (const puchar *) ("abc"), 3);
 	hash_str = p_crypto_hash_get_string (md5_hash);
 	BOOST_CHECK (strcmp (hash_str, "900150983cd24fb0d6963f7d28e17f72") == 0);
+	p_free (hash_str);
 
 	p_crypto_hash_reset (md5_hash);
 	BOOST_REQUIRE (p_crypto_hash_get_string (md5_hash) == NULL);
 
+	p_crypto_hash_update (md5_hash, (const puchar *) "abcdefghijklm", 13);
+	p_crypto_hash_update (md5_hash, (const puchar *) "nopqrstuvwxyz", 13);
+	hash_str = p_crypto_hash_get_string (md5_hash);
+	BOOST_CHECK (strcmp (hash_str, "c3fcd3d76192e4007dfb496cca67e13b") == 0);
 	p_free (hash_str);
+
+	p_crypto_hash_reset (md5_hash);
+	BOOST_REQUIRE (p_crypto_hash_get_string (md5_hash) == NULL);
+
 	p_crypto_hash_free (md5_hash);
 }
 
