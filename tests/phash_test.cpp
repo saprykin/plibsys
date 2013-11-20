4a72f96db74ed7dc5172a9aa6ca104eef1ee725d
Alexander Saprykin <xelfium@gmail.com>
2013-11-20 18:03:21 +0400

    tests: Add MD5 test cases from the NIST site

--- tests/phash_test.cpp
+++ tests/phash_test.cpp
@@ -12,6 +12,7 @@ BOOST_AUTO_TEST_CASE (md5_test)
 {
 	PCryptoHash	*md5_hash;
 	pchar		*hash_str;
+	int		i;
 
 	md5_hash = p_crypto_hash_new (P_CRYPTO_HASH_TYPE_MD5);
 
@@ -27,10 +28,20 @@ BOOST_AUTO_TEST_CASE (md5_test)
 	p_crypto_hash_reset (md5_hash);
 	BOOST_REQUIRE (p_crypto_hash_get_string (md5_hash) == NULL);
 
-	p_crypto_hash_update (md5_hash, (const puchar *) "abcdefghijklm", 13);
-	p_crypto_hash_update (md5_hash, (const puchar *) "nopqrstuvwxyz", 13);
+	p_crypto_hash_update (md5_hash,
+			      (const puchar *) "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+			      56);
 	hash_str = p_crypto_hash_get_string (md5_hash);
-	BOOST_CHECK (strcmp (hash_str, "c3fcd3d76192e4007dfb496cca67e13b") == 0);
+	BOOST_CHECK (strcmp (hash_str, "8215ef0796a20bcaaae116d3876c664a") == 0);
+	p_free (hash_str);
+
+	p_crypto_hash_reset (md5_hash);
+	BOOST_REQUIRE (p_crypto_hash_get_string (md5_hash) == NULL);
+
+	for (i = 0; i < 1000000; ++i)
+		p_crypto_hash_update (md5_hash, (const puchar *) "a", 1);
+	hash_str = p_crypto_hash_get_string (md5_hash);
+	BOOST_CHECK (strcmp (hash_str, "7707d6ae4e027c70eea2a935c2296f21") == 0);
 	p_free (hash_str);
 
 	p_crypto_hash_reset (md5_hash);
