3204719581ad99228b323f2c8550721bd5cd2e7f
Alexander Saprykin <xelfium@gmail.com>
2013-11-23 12:06:04 +0300

    tests: Add test for PCondVariable module

---
+++ tests/pcondvariable_test.cpp
@@ -0,0 +1,122 @@
+#define BOOST_TEST_MODULE pcondvariable_test
+
+#include "plib.h"
+
+#include <boost/test/included/unit_test.hpp>
+
+#define PCONDTEST_MAX_QUEUE 10
+
+static pint thread_wakeups = 0;
+static pint thread_queue = 0;
+static PCondVariable *queue_empty_cond = NULL;
+static PCondVariable *queue_full_cond = NULL;
+static PMutex *cond_mutex = NULL;
+volatile static pboolean is_working = TRUE;
+
+static void * producer_test_thread (void *)
+{
+	while (is_working == TRUE) {
+		if (!p_mutex_lock (cond_mutex)) {
+			is_working = FALSE;
+			p_uthread_exit (1);
+		}
+
+		if (thread_queue == PCONDTEST_MAX_QUEUE) {
+			if (!p_cond_variable_wait (queue_empty_cond, cond_mutex)) {
+				is_working = FALSE;
+				p_uthread_exit (1);
+			}
+		}
+
+		++thread_queue;
+		++thread_wakeups;
+
+		if (!p_mutex_unlock (cond_mutex)) {
+			is_working = FALSE;
+			p_uthread_exit (1);
+		}
+
+		if (!p_cond_variable_signal (queue_full_cond)) {
+			is_working = FALSE;
+			p_uthread_exit (1);
+		}
+	}
+
+	p_uthread_exit (0);
+}
+
+static void * consumer_test_thread (void *)
+{
+	while (is_working == TRUE) {
+		if (!p_mutex_lock (cond_mutex)) {
+			is_working = FALSE;
+			p_uthread_exit (1);
+		}
+
+		if (thread_queue == 0) {
+			if (!p_cond_variable_wait (queue_full_cond, cond_mutex)) {
+				is_working = FALSE;
+				p_uthread_exit (1);
+			}
+		}
+
+		--thread_queue;
+		++thread_wakeups;
+
+		if (!p_mutex_unlock (cond_mutex)) {
+			is_working = FALSE;
+			p_uthread_exit (1);
+		}
+
+		if (!p_cond_variable_signal (queue_empty_cond)) {
+			is_working = FALSE;
+			p_uthread_exit (1);
+		}
+	}
+
+	p_uthread_exit (0);
+}
+
+BOOST_AUTO_TEST_SUITE (BOOST_TEST_MODULE)
+
+BOOST_AUTO_TEST_CASE (pcondvariable_general_test)
+{
+	PUThread	*thr1, *thr2;
+
+	BOOST_REQUIRE (p_cond_variable_broadcast (queue_empty_cond) == FALSE);
+	BOOST_REQUIRE (p_cond_variable_signal (queue_empty_cond) == FALSE);
+	BOOST_REQUIRE (p_cond_variable_wait (queue_empty_cond, cond_mutex) == FALSE);
+
+	queue_empty_cond = p_cond_variable_new ();
+	BOOST_REQUIRE (queue_empty_cond != NULL);
+	queue_full_cond = p_cond_variable_new ();
+	BOOST_REQUIRE (queue_full_cond != NULL);
+	cond_mutex = p_mutex_new ();
+	BOOST_REQUIRE (cond_mutex != NULL);
+
+	thr1 = p_uthread_create ((PUThreadFunc) producer_test_thread, NULL, true);
+	BOOST_REQUIRE (thr1 != NULL);
+
+	thr2 = p_uthread_create ((PUThreadFunc) consumer_test_thread, NULL, true);
+	BOOST_REQUIRE (thr2 != NULL);
+
+	BOOST_REQUIRE (p_cond_variable_broadcast (queue_empty_cond) == TRUE);
+	BOOST_REQUIRE (p_cond_variable_broadcast (queue_full_cond) == TRUE);
+
+	p_uthread_sleep (4000);
+
+	is_working = FALSE;
+
+	BOOST_CHECK (p_uthread_join (thr1) == 0);
+	BOOST_CHECK (p_uthread_join (thr2) == 0);
+
+	BOOST_REQUIRE (thread_wakeups > 0);
+
+	p_uthread_free (thr1);
+	p_uthread_free (thr2);
+	p_cond_variable_free (queue_empty_cond);
+	p_cond_variable_free (queue_full_cond);
+	p_mutex_free (cond_mutex);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
