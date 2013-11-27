5d033755f706cbd9b2ac90c9d94e2c47b3e73a6a
Alexander Saprykin <xelfium@gmail.com>
2013-11-27 20:45:43 +0400

    Add support for SunOS (Solaris) platform

---
+++ platforms/sunos-x86-gcc/pplatform.h
@@ -0,0 +1,24 @@
+#ifndef __PPLATFORM_H__
+#define __PPLATFORM_H__
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <memory.h>
+#include <netinet/in.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#endif /* __PPLATFORM_H__ */
