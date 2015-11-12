7d813f2e4413d00407ae16827ebeb06a7f06f836
Alexander Saprykin <xelfium@gmail.com>
2015-11-13 00:21:15 +0300

    Update copyright years

--- src/pshm.c
+++ src/pshm.c
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2010 Alexander Saprykin <xelfium@gmail.com>
+ * Copyright (C) 2010-2015 Alexander Saprykin <xelfium@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
