d39f44dfdb05b03d1f75164be300a6d3aafb0bdf
Alexander Saprykin <xelfium@gmail.com>
2016-01-18 13:53:42 +0300

    Remove unused header

--- src/psemaphore-win.c
+++ src/psemaphore-win.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2013 Alexander Saprykin <xelfium@gmail.com>
+ * Copyright (C) 2010-2016 Alexander Saprykin <xelfium@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,6 @@
 #include "plib-private.h"
 #include "pmem.h"
 #include "psemaphore.h"
-#include "psha1.h"
 
 #include <stdlib.h>
 #include <string.h>
