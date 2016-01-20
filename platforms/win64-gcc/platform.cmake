24b80a3969748beccce772c8fa847037fd5f5b1e
Alexander Saprykin <xelfium@gmail.com>
2016-01-20 18:44:53 +0300

    Add support for MinGW x64 platform

---
+++ platforms/win64-gcc/platform.cmake
@@ -0,0 +1,21 @@
+set (PLIB_THREAD_MODEL win)
+set (PLIB_IPC_MODEL win)
+set (PLIB_TIME_PROFILER_MODEL win)
+set (PLIB_DIR_MODEL win)
+set (PLIB_PLATFORM_CONFIG_PATH "pplatform.h")
+
+# Ensure MSVC-compatible struct packing convention is used
+# What flag to depends on gcc version: gcc3 uses '-mms-bitfields', while
+# gcc2 uses '-fnative-struct'.
+message (STATUS "Checking for MSVC-compatible code compilation flags")
+exec_program (gcc ARGS --version OUTPUT_VARIABLE CMAKE_C_COMPILER_VERSION)
+
+if (CMAKE_C_COMPILER_VERSION MATCHES ".*2\\.[0-9].*")
+	set (PLIB_PLATFORM_CFLAGS "-fnative-struct")
+	message (STATUS "Checking for MSVC-compatible code compile flags -fnative-struct")
+else()
+	set (PLIB_PLATFORM_CFLAGS "-mms-bitfields")
+	message (STATUS "Checking for MSVC-compatible code compile flags -mms-bitfields")
+endif()
+
+set (PLIB_PLATFORM_LINK_LIBRARIES ws2_32)
---
+++ platforms/win64-gcc/pplatform.h
@@ -0,0 +1,19 @@
+#ifndef __PPLATFORM_H__
+#define __PPLATFORM_H__
+
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <memory.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#endif /* __PPLATFORM_H__ */
