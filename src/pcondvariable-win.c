e6d9458343fcebb1a8c8ff880643e71efe368035
Alexander Saprykin <xelfium@gmail.com>
2016-01-28 14:20:00 +0300

    Use native condition variables on Windows if possible (Vista and higher)

--- src/pcondvariable-win.c
+++ src/pcondvariable-win.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2010-2016 Alexander Saprykin <xelfium@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -22,21 +22,21 @@
  * See: https://github.com/python/cpython/blob/master/Python/condvar.h
  */
 
-/* TODO: Use native conditional variable if available */
-
 #include "pmem.h"
 #include "pcondvariable.h"
 
 #include <stdlib.h>
-#include <time.h>
-#include <string.h>
 
 #include <winsock2.h>
 #include <windows.h>
 
 struct _PCondVariable {
-	HANDLE	waiters_sema;
-	pint	waiters_count;
+#ifdef PLIB_HAS_VISTA_CV
+	CONDITION_VARIABLE	cv;
+#else
+	HANDLE			waiters_sema;
+	pint			waiters_count;
+#endif
 };
 
 P_LIB_API PCondVariable *
@@ -49,6 +49,9 @@ p_cond_variable_new (void)
 		return NULL;
 	}
 
+#ifdef PLIB_HAS_VISTA_CV
+	InitializeConditionVariable (&ret->cv);
+#else
 	ret->waiters_count	= 0;
 	ret->waiters_sema	= CreateSemaphore (NULL, 0, MAXLONG, NULL);
 
@@ -57,7 +60,8 @@ p_cond_variable_new (void)
 		p_free (ret);
 		return NULL;
 	}
-	
+#endif
+
 	return ret;
 }
 
@@ -67,7 +71,9 @@ p_cond_variable_free (PCondVariable *cond)
 	if (cond == NULL)
 		return;
 
+#ifndef PLIB_HAS_VISTA_CV
 	CloseHandle (cond->waiters_sema);
+#endif
 
 	p_free (cond);
 }
@@ -76,11 +82,16 @@ P_LIB_API pboolean
 p_cond_variable_wait (PCondVariable	*cond,
 		      PMutex		*mutex)
 {
+#ifndef PLIB_HAS_VISTA_CV
 	DWORD wait;
+#endif
 
 	if (cond == NULL || mutex == NULL)
 		return FALSE;
-	
+
+#ifdef PLIB_HAS_VISTA_CV
+	return SleepConditionVariableCS (&cond->cv, (PCRITICAL_SECTION) mutex, INFINITE) != 0 ? TRUE : FALSE;
+#else
 	cond->waiters_count++;
 
 	p_mutex_unlock (mutex);
@@ -94,6 +105,7 @@ p_cond_variable_wait (PCondVariable	*cond,
 		return FALSE;
 
 	return wait == WAIT_OBJECT_0 ? TRUE : FALSE;
+#endif
 }
 
 P_LIB_API pboolean
@@ -101,11 +113,15 @@ p_cond_variable_signal (PCondVariable *cond)
 {
 	if (cond == NULL)
 		return FALSE;
-	
+
+#ifdef PLIB_HAS_VISTA_CV
+	WakeConditionVariable (&cond->cv);
+#else
 	if (cond->waiters_count > 0) {
 		cond->waiters_count--;
 		return ReleaseSemaphore (cond->waiters_sema, 1, 0) != 0 ? TRUE : FALSE;
 	}
+#endif
 
 	return TRUE;
 }
@@ -113,17 +129,23 @@ p_cond_variable_signal (PCondVariable *cond)
 P_LIB_API pboolean
 p_cond_variable_broadcast (PCondVariable *cond)
 {
+#ifndef PLIB_HAS_VISTA_CV
 	pint waiters;
+#endif
 
 	if (cond == NULL)
 		return FALSE;
 
+#ifdef PLIB_HAS_VISTA_CV
+	WakeAllConditionVariable (&cond->cv);
+#else
 	waiters = cond->waiters_count;
 
-	if (waiters > 0) {		
+	if (waiters > 0) {
 		cond->waiters_count = 0;
-		ReleaseSemaphore (cond->waiters_sema, waiters, 0) != 0 ? TRUE : FALSE;
+		return ReleaseSemaphore (cond->waiters_sema, waiters, 0) != 0 ? TRUE : FALSE;
 	}
+#endif
 
 	return TRUE;
 }
