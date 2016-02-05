f2265a08468f176b0ac6d7dd6bb3537744710e1e
Alexander Saprykin <xelfium@gmail.com>
2016-02-05 13:40:12 +0300

    Fix Oracle Solaris Studio compiler detection when using C++ compiler (not C)

--- src/pmacros.h
+++ src/pmacros.h
@@ -1,5 +1,5 @@
-/* 
- * Copyright (C) 2010-2014 Alexander Saprykin <xelfium@gmail.com>
+/*
+ * Copyright (C) 2010-2016 Alexander Saprykin <xelfium@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 
 /**
  * @file pmacros.h
- * @brief Miscellaneous macros 
+ * @brief Miscellaneous macros
  * @author Alexander Saprykin
  *
  * Contains useful macros for detecting host OS, compiler, etc.
@@ -292,7 +292,7 @@
 #    define P_CC_CLANG
 #  endif
 /* Sun WorkShop/Studio */
-#elif defined(__SUNPRO_C)
+#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
 #  define P_CC_SUN
 /* Watcom C++ */
 #elif defined(__WATCOMC__)
