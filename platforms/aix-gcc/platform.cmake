dd083eea7716e6d3092d40f243762a01971f72f2
Alexander Saprykin <xelfium@gmail.com>
2016-02-07 14:15:43 +0300

    cmake: Add GCC linker flags for AIX platform

--- platforms/aix-gcc/platform.cmake
+++ platforms/aix-gcc/platform.cmake
@@ -3,7 +3,8 @@ set (PLIB_IPC_MODEL sysv)
 set (PLIB_TIME_PROFILER_MODEL posix)
 set (PLIB_DIR_MODEL posix)
 
-set (PLIB_PLATFORM_LINK_LIBRARIES c_r pthreads)
+set (PLIB_PLATFORM_LDFLAGS -L/usr/lib/threads)
+set (PLIB_PLATFORM_LINK_LIBRARIES c_r -pthread)
 
 set (PLIB_PLATFORM_DEFINES
         -D_REENTRANT
