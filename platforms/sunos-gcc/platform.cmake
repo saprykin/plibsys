528e131f94a43542567345db1cb964408c44f477
Alexander Saprykin <xelfium@gmail.com>
2016-02-10 19:03:39 +0300

    Add time profiler based on Solaris gethrtime() call

--- platforms/sunos-gcc/platform.cmake
+++ platforms/sunos-gcc/platform.cmake
@@ -1,6 +1,6 @@
 set (PLIB_THREAD_MODEL solaris)
 set (PLIB_IPC_MODEL sysv)
-set (PLIB_TIME_PROFILER_MODEL posix)
+set (PLIB_TIME_PROFILER_MODEL solaris)
 set (PLIB_DIR_MODEL posix)
 
 set (PLIB_PLATFORM_LINK_LIBRARIES socket nsl)
---
+++ src/ptimeprofiler-solaris.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2016 Alexander Saprykin <xelfium@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+ */
+
+#include "ptimeprofiler.h"
+#include "pmem.h"
+
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+
+struct _PTimeProfiler {
+	puint64	counter;
+};
+
+P_LIB_API PTimeProfiler *
+p_time_profiler_new ()
+{
+	PTimeProfiler	*ret;
+
+	if ((ret = p_malloc0 (sizeof (PTimeProfiler))) == NULL)
+		return NULL;
+
+	ret->counter = (puint64) gethrtime ();
+
+	return ret;
+}
+
+P_LIB_API void
+p_time_profiler_reset (PTimeProfiler *profiler)
+{
+	if (profiler == NULL)
+		return;
+
+	profiler->counter = (puint64) gethrtime ();
+}
+
+P_LIB_API puint64
+p_time_profiler_elapsed_usecs (const PTimeProfiler *profiler)
+{
+	if (profiler == NULL)
+		return 0;
+
+	return (((puint64) gethrtime ()) - profiler->counter) / 1000;
+}
+
+P_LIB_API void
+p_time_profiler_free (PTimeProfiler *profiler)
+{
+	if (profiler == NULL)
+		return;
+
+	p_free (profiler);
+}
