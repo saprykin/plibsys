777d5d3087e882734cef622b0dd3d89b7c1f8419
Alexander Saprykin <xelfium@gmail.com>
2016-02-25 12:05:43 +0300

    Add domain-based error codes and calls to convert system error codes

--- src/perror.c
+++ src/perror.c
@@ -20,11 +20,395 @@
 #include "perror.h"
 #include "pstring.h"
 
+#ifndef P_OS_WIN
+#include <errno.h>
+#else
+#include <windows.h>
+#endif
+
 struct _PError {
 	pint	code;
 	pint	native_code;
 	pchar	*message;
 };
+
+PErrorIO
+__p_error_get_last_io ()
+{
+	pint err_code;
+
+#ifdef P_OS_WIN
+	err_code = (pint) WSAGetLastError ();
+#else
+	err_code = errno;
+#endif
+
+	switch (err_code) {
+	case 0:
+		return P_ERROR_IO_NONE;
+#ifdef P_OS_WIN
+#  ifdef WSAEADDRINUSE
+	case WSAEADDRINUSE:
+		return P_ERROR_IO_ADDRESS_IN_USE;
+#  endif
+#  ifdef WSAEWOULDBLOCK
+	case WSAEWOULDBLOCK:
+		return P_ERROR_IO_WOULD_BLOCK;
+#  endif
+#  ifdef WSAEACCES
+	case WSAEACCES:
+		return P_ERROR_IO_ACCESS_DENIED;
+#  endif
+#  ifdef WSA_INVALID_HANDLE
+	case WSA_INVALID_HANDLE:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+#  ifdef WSA_INVALID_PARAMETER
+	case WSA_INVALID_PARAMETER:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+#  ifdef WSAEBADF
+	case WSAEBADF:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+#  ifdef WSAENOTSOCK
+	case WSAENOTSOCK:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+#  ifdef WSAEINVAL
+	case WSAEINVAL:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+#  ifdef WSAESOCKTNOSUPPORT
+	case WSAESOCKTNOSUPPORT:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+#  ifdef WSAEOPNOTSUPP
+	case WSAEOPNOTSUPP:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+#  ifdef WSAEPFNOSUPPORT
+	case WSAEPFNOSUPPORT:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+#  ifdef WSAEAFNOSUPPORT
+	case WSAEAFNOSUPPORT:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+#  ifdef WSAEPROTONOSUPPORT
+	case WSAEPROTONOSUPPORT:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+#  ifdef WSAECANCELLED
+	case WSAECANCELLED:
+		return P_ERROR_IO_ABORTED;
+#  endif
+#else /* !P_OS_WIN */
+#  ifdef EACCES
+	case EACCES:
+		return P_ERROR_IO_ACCESS_DENIED;
+#  endif
+
+#  ifdef EPERM
+	case EPERM:
+		return P_ERROR_IO_ACCESS_DENIED;
+#  endif
+
+#  ifdef ENOMEM
+	case ENOMEM:
+		return P_ERROR_IO_NO_RESOURCES;
+#  endif
+
+#  ifdef ENOSR
+	case ENOSR:
+		return P_ERROR_IO_NO_RESOURCES;
+#  endif
+
+#  ifdef ENOBUFS
+	case ENOBUFS:
+		return P_ERROR_IO_NO_RESOURCES;
+#  endif
+
+#  ifdef ENFILE
+	case ENFILE:
+		return P_ERROR_IO_NO_RESOURCES;
+#  endif
+
+#  ifdef EMFILE
+	case EMFILE:
+		return P_ERROR_IO_NO_RESOURCES;
+#  endif
+
+#  ifdef EINVAL
+	case EINVAL:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+
+#  ifdef EBADF
+	case EBADF:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+
+#  ifdef ENOTSOCK
+	case ENOTSOCK:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+
+#  ifdef EFAULT
+	case EFAULT:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+
+#  ifdef EPROTOTYPE
+	case EPROTOTYPE:
+		return P_ERROR_IO_INVALID_ARGUMENT;
+#  endif
+
+			/* On Linux these errors can have same codes */
+#  if defined(ENOTSUP) && (!defined(EOPNOTSUPP) || ENOTSUP != EOPNOTSUPP)
+	case ENOTSUP:
+			return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+
+#  ifdef ENOPROTOOPT
+	case ENOPROTOOPT:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+
+#  ifdef EPROTONOSUPPORT
+	case EPROTONOSUPPORT:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+
+#  ifdef EAFNOSUPPORT
+	case EAFNOSUPPORT:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+
+#  ifdef EOPNOTSUPP
+	case EOPNOTSUPP:
+		return P_ERROR_IO_NOT_SUPPORTED;
+#  endif
+
+#  ifdef EADDRNOTAVAIL
+	case EADDRNOTAVAIL:
+		return P_ERROR_IO_NOT_AVAILABLE;
+#  endif
+
+#  ifdef ENETUNREACH
+	case ENETUNREACH:
+		return P_ERROR_IO_NOT_AVAILABLE;
+#  endif
+
+#  ifdef ENETDOWN
+	case ENETDOWN:
+		return P_ERROR_IO_NOT_AVAILABLE;
+#  endif
+
+#  ifdef EHOSTDOWN
+	case EHOSTDOWN:
+		return P_ERROR_IO_NOT_AVAILABLE;
+#  endif
+
+#  ifdef ENONET
+	case ENONET:
+		return P_ERROR_IO_NOT_AVAILABLE;
+#  endif
+
+#  ifdef EHOSTUNREACH
+	case EHOSTUNREACH:
+		return P_ERROR_IO_NOT_AVAILABLE;
+#  endif
+
+#  ifdef EINPROGRESS
+	case EINPROGRESS:
+		return P_ERROR_IO_CONNECTING;
+#  endif
+
+#  ifdef EALREADY
+	case EALREADY:
+		return P_ERROR_IO_CONNECTING;
+#  endif
+
+#  ifdef EISCONN
+	case EISCONN:
+		return P_ERROR_IO_CONNECTED;
+#  endif
+
+#  ifdef ECONNREFUSED
+	case ECONNREFUSED:
+		return P_ERROR_IO_CONNECTION_REFUSED;
+#  endif
+
+#  ifdef ENOTCONN
+	case ENOTCONN:
+		return P_ERROR_IO_NOT_CONNECTED;
+#  endif
+
+#  ifdef ECONNABORTED
+	case ECONNABORTED:
+		return P_ERROR_IO_ABORTED;
+#  endif
+
+#  ifdef EADDRINUSE
+	case EADDRINUSE:
+		return P_ERROR_IO_ADDRESS_IN_USE;
+#  endif
+
+#  ifdef ETIMEDOUT
+	case ETIMEDOUT:
+		return P_ERROR_IO_TIMED_OUT;
+#  endif
+
+	/* Some magic to deal with EWOULDBLOCK and EAGAIN.
+	 * Apparently on HP-UX these are actually defined to different values,
+	 * but on Linux, for example, they are the same. */
+#  if defined(EWOULDBLOCK) && defined(EAGAIN) && EWOULDBLOCK == EAGAIN
+	/* We have both and they are the same: only emit one case. */
+	case EAGAIN:
+		return P_ERROR_IO_WOULD_BLOCK;
+#  else
+	/* Else: consider each of them separately. This handles both the
+	 * case of having only one and the case where they are different values. */
+#    ifdef EAGAIN
+	case EAGAIN:
+		return P_ERROR_IO_WOULD_BLOCK;
+#    endif
+
+#    ifdef EWOULDBLOCK
+	case EWOULDBLOCK:
+		return P_ERROR_IO_WOULD_BLOCK;
+#    endif
+#  endif
+#endif /* !P_OS_WIN */
+	default:
+		return P_ERROR_IO_FAILED;
+	}
+}
+
+PErrorIPC
+__p_error_get_last_ipc ()
+{
+	pint err_code;
+
+#ifdef P_OS_WIN
+	err_code = (pint) GetLastError ();
+#else
+	err_code = errno;
+#endif
+
+	switch (err_code) {
+	case 0:
+		return P_ERROR_IPC_NONE;
+#ifdef P_OS_WIN
+	case ERROR_SEM_OWNER_DIED:
+		return P_ERROR_IPC_NOT_EXISTS;
+	case ERROR_SEM_NOT_FOUND:
+		return P_ERROR_IPC_NOT_EXISTS;
+	case ERROR_SEM_USER_LIMIT:
+		return P_ERROR_IPC_NO_RESOURCES;
+	case ERROR_TOO_MANY_SEMAPHORES:
+		return P_ERROR_IPC_NO_RESOURCES;
+	case ERROR_ACCESS_DENIED:
+		return P_ERROR_IPC_ACCESS;
+	case ERROR_EXCL_SEM_ALREADY_OWNED:
+		return P_ERROR_IPC_ACCESS;
+	case ERROR_TOO_MANY_SEM_REQUESTS:
+		return P_ERROR_IPC_NO_RESOURCES;
+	case ERROR_TOO_MANY_POSTS:
+		return P_ERROR_IPC_NO_RESOURCES;
+	case ERROR_INVALID_HANDLE:
+		return P_ERROR_IPC_INVALID_ARGUMENT;
+#else /* !P_OS_WIN */
+#  ifdef EACCES
+	case EACCES:
+		return P_ERROR_IPC_ACCESS;
+#  endif
+
+#  ifdef EPERM
+	case EPERM:
+		return P_ERROR_IPC_ACCESS;
+#  endif
+
+#  ifdef EEXIST
+	case EEXIST:
+		return P_ERROR_IPC_EXISTS;
+#  endif
+
+#  ifdef E2BIG
+	case E2BIG:
+		return P_ERROR_IPC_INVALID_ARGUMENT;
+#  endif
+
+#  ifdef EFBIG
+	case EFBIG:
+		return P_ERROR_IPC_INVALID_ARGUMENT;
+#  endif
+
+#  ifdef EINVAL
+	case EINVAL:
+		return P_ERROR_IPC_INVALID_ARGUMENT;
+#  endif
+
+#  ifdef ERANGE
+	case ERANGE:
+		return P_ERROR_IPC_INVALID_ARGUMENT;
+#  endif
+
+#  ifdef ENOMEM
+	case ENOMEM:
+		return P_ERROR_IPC_NO_RESOURCES;
+#  endif
+
+#  ifdef EMFILE
+	case EMFILE:
+		return P_ERROR_IPC_NO_RESOURCES;
+#  endif
+
+#  ifdef ENFILE
+	case ENFILE:
+		return P_ERROR_IPC_NO_RESOURCES;
+#  endif
+
+#  ifdef ENOSPC
+	case ENOSPC:
+		return P_ERROR_IPC_NO_RESOURCES;
+#  endif
+
+#  ifdef EIDRM
+	case EIDRM:
+		return P_ERROR_IPC_NOT_EXISTS;
+#  endif
+
+#  ifdef ENOENT
+	case ENOENT:
+		return P_ERROR_IPC_NOT_EXISTS;
+#  endif
+
+#  ifdef EOVERFLOW
+	case EOVERFLOW:
+		return P_ERROR_IPC_OVERFLOW;
+#  endif
+
+#  ifdef ENOSYS
+	case ENOSYS:
+		return P_ERROR_IPC_NOT_IMPLEMENTED;
+#  endif
+
+#  ifdef EDEADLK
+	case EDEADLK:
+		return P_ERROR_IPC_DEADLOCK;
+#  endif
+
+#  ifdef ENAMETOOLONG
+	case ENAMETOOLONG:
+		return P_ERROR_IPC_NAMETOOLONG;
+#  endif
+#endif /* !P_OS_WIN */
+	default:
+		return P_ERROR_IPC_FAILED;
+	}
+}
 
 P_LIB_API PError *
 p_error_new ()
@@ -79,6 +463,20 @@ p_error_get_native_code	(PError	*error)
 		return 0;
 
 	return error->native_code;
+}
+
+P_LIB_API PErrorDomain
+p_error_get_domain (PError *error)
+{
+	if (error == NULL)
+		return P_ERROR_DOMAIN_NONE;
+
+	if (error->code >= (pint) P_ERROR_DOMAIN_IPC)
+		return P_ERROR_DOMAIN_IPC;
+	else if (error->code >= (pint) P_ERROR_DOMAIN_IO)
+		return P_ERROR_DOMAIN_IO;
+	else
+		return P_ERROR_DOMAIN_NONE;
 }
 
 P_LIB_API PError *
--- src/perror.h
+++ src/perror.h
@@ -49,6 +49,51 @@
 
 P_BEGIN_DECLS
 
+/** Enum with error domains */
+typedef enum _PErrorDomain {
+	P_ERROR_DOMAIN_NONE		= 0,	/**< No domain was specified.				*/
+	P_ERROR_DOMAIN_IO		= 500,	/**< Input/output domain.				*/
+	P_ERROR_DOMAIN_IPC		= 600	/**< Interprocess communication domain.			*/
+} PErrorDomain;
+
+/** Enum with IO errors */
+typedef enum _PErrorIO {
+	P_ERROR_IO_NONE			= 500,	/**< No error.						*/
+	P_ERROR_IO_NO_RESOURCES		= 501,	/**< OS hasn't enough resources.			*/
+	P_ERROR_IO_NOT_AVAILABLE	= 502,	/**< Resource isn't available.				*/
+	P_ERROR_IO_ACCESS_DENIED	= 503,	/**< Access denied.					*/
+	P_ERROR_IO_CONNECTED		= 504,	/**< Already connected.					*/
+	P_ERROR_IO_CONNECTING		= 505,	/**< Connection in progress.				*/
+	P_ERROR_IO_ABORTED		= 506,	/**< Operation aborted.					*/
+	P_ERROR_IO_INVALID_ARGUMENT	= 507,	/**< Invalid argument specified.			*/
+	P_ERROR_IO_NOT_SUPPORTED	= 508,	/**< Operation not supported.				*/
+	P_ERROR_IO_TIMED_OUT		= 509,	/**< Operation timed out.				*/
+	P_ERROR_IO_WOULD_BLOCK		= 510,	/**< Operation cannot be completed immediatly.		*/
+	P_ERROR_IO_ADDRESS_IN_USE	= 511,	/**< Address is already under usage.			*/
+	P_ERROR_IO_CONNECTION_REFUSED	= 512,	/**< Connection refused.				*/
+	P_ERROR_IO_NOT_CONNECTED	= 513,	/**< Connection required first.				*/
+	P_ERROR_IO_FAILED		= 514	/**< General error.					*/
+} PErrorIO;
+
+/** Enum with IPC errors */
+typedef enum _PErrorIPC {
+	P_ERROR_IPC_NONE		= 600,	/**< No error.						*/
+	P_ERROR_IPC_ACCESS		= 601,	/**< Not enough rights to access object or its key.	*/
+	P_ERROR_IPC_EXISTS		= 602,	/**< Object already exists and no proper open flags
+						     were specified.					*/
+	P_ERROR_IPC_NOT_EXISTS		= 603,	/**< Object doesn't exist or was removed before, and
+						     no proper create flags were specified.		*/
+	P_ERROR_IPC_NO_RESOURCES	= 604,	/**< Not enough system resources or memory to perform
+						     operation.						*/
+	P_ERROR_IPC_OVERFLOW		= 605,	/**< Semaphore value overflow.				*/
+	P_ERROR_IPC_NAMETOOLONG		= 606,	/**< Object name is too long.				*/
+	P_ERROR_IPC_INVALID_ARGUMENT	= 607,	/**< Invalid argument (parameter) specified.		*/
+	P_ERROR_IPC_NOT_IMPLEMENTED	= 608,	/**< Operation not implemented (for example when using
+						     some file systems).				*/
+	P_ERROR_IPC_DEADLOCK		= 609,	/**< Deadlock detected.					*/
+	P_ERROR_IPC_FAILED		= 610	/**< General error.					*/
+} PErrorIPC;
+
 /** Opaque data structure for an error object. */
 typedef struct _PError PError;
 
@@ -98,6 +143,14 @@ P_LIB_API pint		p_error_get_code	(PError		*error);
  * i.e. when internal library call failed. Do not rely on this code.
  */
 P_LIB_API pint		p_error_get_native_code	(PError		*error);
+
+/**
+ * @brief Gets error domain.
+ * @param error #PError object to get domain from.
+ * @return Error domain in case of success, #P_ERROR_DOMAIN_NONE otherwise.
+ * @since 0.0.1
+ */
+P_LIB_API PErrorDomain	p_error_get_domain	(PError		*error);
 
 /**
  * @brief Creates a copy of a #PError object.
