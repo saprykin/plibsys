33b1756019dda924f67d745449a61974d5aff20a
Alexander Saprykin <xelfium@gmail.com>
2016-03-10 01:40:19 +0300

    cmake: Format SCO OpenServer note about FSU Pthreads library

--- platforms/scosv-gcc/platform.cmake
+++ platforms/scosv-gcc/platform.cmake
@@ -3,14 +3,17 @@ set (PLIB_IPC_MODEL sysv)
 set (PLIB_TIME_PROFILER_MODEL posix)
 set (PLIB_DIR_MODEL posix)
 
-set (PLIB_PLATFORM_LINK_LIBRARIES socket nsl gthreads malloc)
+set (PLIB_PLATFORM_LINK_LIBRARIES socket nsl gthreads)
 
 set (PLIB_PLATFORM_DEFINES
         -D_REENTRANT
         -D_SIMPLE_R
 )
 
-message (STATUS "You need a working port of FSU Pthreads in order to
-                 compile with multi-threading support. Please refer to
-                 http://moss.csc.ncsu.edu/~mueller/pthreads/ for more
-                 details.")
+message ("
+         You need a working port of FSU Pthreads in order to
+         compile with multi-threading support. Please refer to
+         http://moss.csc.ncsu.edu/~mueller/pthreads/ for more
+         details. Make sure that it is compiled with thread-safe
+         memory allocation (usually -DMALLOC macro definition).
+         ")
