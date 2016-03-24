9514b71db0ee3b27dca5eaaadfd8db5faf60ab2b
Alexander Saprykin <xelfium@gmail.com>
2016-03-24 13:29:11 +0300

    appveyor: Fix spaces in MSYS path

--- contrib/appveyor/build_msys.sh
+++ contrib/appveyor/build_msys.sh
@@ -1,4 +1,4 @@
 cd /c/projects/plib-build
-/c/Program Files\ (x86)/CMake/bin/cmake -DCMAKE_BUILD_TYPE=$configuration -G"$CMAKE_GENERATOR" $BOOST_ARGS /c/projects/plib
+/c/Program\ Files\ (x86)/CMake/bin/cmake -DCMAKE_BUILD_TYPE=$configuration -G"$CMAKE_GENERATOR" $BOOST_ARGS /c/projects/plib
 make
-/c/Program Files\ (x86)/CMake/bin/ctest
+/c/Program\ Files\ (x86)/CMake/bin/ctest
