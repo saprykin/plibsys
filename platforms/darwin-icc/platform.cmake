22f498bbd8ee070196cf458ac78cb1834be418c4
Alexander Saprykin <xelfium@gmail.com>
2016-03-26 01:53:29 +0300

    cmake: Add support for Intel compiler on OS X

---
+++ platforms/darwin-icc/platform.cmake
@@ -0,0 +1,10 @@
+set (PLIB_THREAD_MODEL posix)
+set (PLIB_IPC_MODEL posix)
+set (PLIB_TIME_PROFILER_MODEL posix)
+set (PLIB_DIR_MODEL posix)
+
+set (PLIB_PLATFORM_LINK_LIBRARIES -pthread imf svml irng intlc)
+
+set (PLIB_PLATFORM_DEFINES
+        -D_REENTRANT
+)
