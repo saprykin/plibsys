508ab45bd40cd8c2c60aec5c1d40248b24fa7e67
Alexander Saprykin <xelfium@gmail.com>
2016-04-06 00:08:28 +0300

    tests: Refactor PHashTable with more stress testing

--- tests/phashtable_test.cpp
+++ tests/phashtable_test.cpp
@@ -24,6 +24,9 @@
 
 #include "plib.h"
 
+#include <stdlib.h>
+#include <time.h>
+
 #ifdef PLIB_TESTS_STATIC
 #  include <boost/test/included/unit_test.hpp>
 #else
@@ -37,20 +40,29 @@ static int test_hash_table_values (pconstpointer a, pconstpointer b)
 
 BOOST_AUTO_TEST_SUITE (BOOST_TEST_MODULE)
 
+#define PHASHTABLE_STRESS_COUNT	10000
+
+BOOST_AUTO_TEST_CASE (phashtable_invalid_test)
+{
+	p_lib_init ();
+
+	BOOST_CHECK (p_hash_table_keys (NULL) == NULL);
+	BOOST_CHECK (p_hash_table_values (NULL) == NULL);
+	BOOST_CHECK (p_hash_table_lookup (NULL, NULL) == NULL);
+	BOOST_CHECK (p_hash_table_lookup_by_value (NULL, NULL, NULL) == NULL);
+	p_hash_table_insert (NULL, NULL, NULL);
+	p_hash_table_remove (NULL, NULL);
+	p_hash_table_free (NULL);
+
+	p_lib_shutdown ();
+}
+
 BOOST_AUTO_TEST_CASE (phashtable_general_test)
 {
 	PHashTable	*table = NULL;
 	PList		*list = NULL;
 
 	p_lib_init ();
-
-	BOOST_CHECK (p_hash_table_keys (table) == NULL);
-	BOOST_CHECK (p_hash_table_values (table) == NULL);
-	BOOST_CHECK (p_hash_table_lookup (table, NULL) == NULL);
-	BOOST_CHECK (p_hash_table_lookup_by_value (table, NULL, NULL) == NULL);
-	p_hash_table_insert (table, NULL, NULL);
-	p_hash_table_remove (table, NULL);
-	p_hash_table_free (table);
 
 	table = p_hash_table_new ();
 	BOOST_REQUIRE (table != NULL);
@@ -80,6 +92,19 @@ BOOST_AUTO_TEST_CASE (phashtable_general_test)
 	BOOST_REQUIRE (PPOINTER_TO_INT (list->data) == 1);
 	p_list_free (list);
 
+	/* Replace existing value */
+	p_hash_table_insert (table, PINT_TO_POINTER (1), PINT_TO_POINTER (15));
+	list = p_hash_table_values (table);
+	BOOST_REQUIRE (list != NULL);
+	BOOST_REQUIRE (p_list_length (list) == 1);
+	BOOST_REQUIRE (PPOINTER_TO_INT (list->data) == 15);
+	p_list_free (list);
+	list = p_hash_table_keys (table);
+	BOOST_REQUIRE (list != NULL);
+	BOOST_REQUIRE (p_list_length (list) == 1);
+	BOOST_REQUIRE (PPOINTER_TO_INT (list->data) == 1);
+	p_list_free (list);
+
 	/* More insertion */
 	p_hash_table_insert (table, PINT_TO_POINTER (2), PINT_TO_POINTER (20));
 	p_hash_table_insert (table, PINT_TO_POINTER (3), PINT_TO_POINTER (30));
@@ -89,7 +114,7 @@ BOOST_AUTO_TEST_CASE (phashtable_general_test)
 	BOOST_REQUIRE (p_list_length (list) == 3);
 	BOOST_REQUIRE (PPOINTER_TO_INT (list->data) +
 		       PPOINTER_TO_INT (list->next->data) +
-		       PPOINTER_TO_INT (list->next->next->data) == 60);
+		       PPOINTER_TO_INT (list->next->next->data) == 65);
 	p_list_free (list);
 	list = p_hash_table_keys (table);
 	BOOST_REQUIRE (list != NULL);
@@ -99,7 +124,7 @@ BOOST_AUTO_TEST_CASE (phashtable_general_test)
 		       PPOINTER_TO_INT (list->next->next->data) == 6);
 	p_list_free (list);
 
-	BOOST_CHECK (PPOINTER_TO_INT (p_hash_table_lookup (table, PINT_TO_POINTER (1))) == 10);
+	BOOST_CHECK (PPOINTER_TO_INT (p_hash_table_lookup (table, PINT_TO_POINTER (1))) == 15);
 	BOOST_CHECK (PPOINTER_TO_INT (p_hash_table_lookup (table, PINT_TO_POINTER (2))) == 20);
 	BOOST_CHECK (PPOINTER_TO_INT (p_hash_table_lookup (table, PINT_TO_POINTER (3))) == 30);
 	BOOST_CHECK (p_hash_table_lookup (table, PINT_TO_POINTER (4)) == (ppointer) -1);
@@ -112,7 +137,16 @@ BOOST_AUTO_TEST_CASE (phashtable_general_test)
 	BOOST_REQUIRE (p_list_length (list) == 3);
 	BOOST_REQUIRE (PPOINTER_TO_INT (list->data) +
 		       PPOINTER_TO_INT (list->next->data) +
-		       PPOINTER_TO_INT (list->next->next->data)== 27);
+		       PPOINTER_TO_INT (list->next->next->data) == 27);
+	p_list_free (list);
+
+	list = p_hash_table_lookup_by_value (table,
+					     PINT_TO_POINTER (20),
+					     NULL);
+	BOOST_REQUIRE (list != NULL);
+	BOOST_REQUIRE (p_list_length (list) == 2);
+	BOOST_REQUIRE (PPOINTER_TO_INT (list->data) +
+		       PPOINTER_TO_INT (list->next->data) == 24);
 	p_list_free (list);
 
 	BOOST_REQUIRE (PPOINTER_TO_INT (p_hash_table_lookup (table, PINT_TO_POINTER (22))) == 20);
@@ -142,6 +176,80 @@ BOOST_AUTO_TEST_CASE (phashtable_general_test)
 
 	BOOST_REQUIRE (p_hash_table_keys (table) == NULL);
 	BOOST_REQUIRE (p_hash_table_values (table) == NULL);
+
+	p_hash_table_free (table);
+
+	p_lib_shutdown ();
+}
+
+BOOST_AUTO_TEST_CASE (phashtable_stress_test)
+{
+	p_lib_init ();
+
+	PHashTable *table = p_hash_table_new ();
+	BOOST_REQUIRE (table != NULL);
+
+	srand ((unsigned int) time (NULL));
+
+	int counter = 0;
+
+	pint *keys   = (pint *) p_malloc0 (PHASHTABLE_STRESS_COUNT * sizeof (pint));
+	pint *values = (pint *) p_malloc0 (PHASHTABLE_STRESS_COUNT * sizeof (pint));
+
+	BOOST_REQUIRE (keys != NULL);
+	BOOST_REQUIRE (values != NULL);
+
+	while (counter != PHASHTABLE_STRESS_COUNT) {
+		pint rand_number = rand ();
+
+		if (p_hash_table_lookup (table, PINT_TO_POINTER (rand_number)) != (ppointer) (-1))
+			continue;
+
+		keys[counter]   = rand_number;
+		values[counter] = rand () + 1;
+
+		p_hash_table_insert (table, PINT_TO_POINTER (keys[counter]), PINT_TO_POINTER (values[counter]));
+
+		++counter;
+	}
+
+	for (int i = 0; i < PHASHTABLE_STRESS_COUNT; ++i) {
+		BOOST_CHECK (p_hash_table_lookup (table, PINT_TO_POINTER (keys[i])) ==
+			     PINT_TO_POINTER (values[i]));
+
+		PList *keys_list = p_hash_table_lookup_by_value (table, PINT_TO_POINTER (values[i]), NULL);
+		BOOST_CHECK (p_list_length (keys_list) == 1);
+		BOOST_CHECK (PPOINTER_TO_INT (keys_list->data) == keys[i]);
+		p_list_free (keys_list);
+
+		p_hash_table_remove (table, PINT_TO_POINTER (keys[i]));
+		BOOST_CHECK (p_hash_table_lookup (table, PINT_TO_POINTER (keys[i])) == (ppointer) (-1));
+	}
+
+	BOOST_CHECK (p_hash_table_keys (table) == NULL);
+	BOOST_CHECK (p_hash_table_values (table) == NULL);
+
+	p_free (keys);
+	p_free (values);
+
+	p_hash_table_free (table);
+
+	/* Try to free at once */
+	table = p_hash_table_new ();
+	BOOST_REQUIRE (table != NULL);
+
+	counter = 0;
+
+	while (counter != PHASHTABLE_STRESS_COUNT) {
+		pint rand_number = rand ();
+
+		if (p_hash_table_lookup (table, PINT_TO_POINTER (rand_number)) != (ppointer) (-1))
+			continue;
+
+		p_hash_table_insert (table, PINT_TO_POINTER (rand_number), PINT_TO_POINTER (rand () + 1));
+
+		++counter;
+	}
 
 	p_hash_table_free (table);
 
