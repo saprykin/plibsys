813ee997621570ad6b37fee7edd32df7b8110a90
Alexander Saprykin <xelfium@gmail.com>
2016-04-07 00:38:14 +0300

    tests: Add checks for IPv6 address if available

--- tests/psocketaddress_test.cpp
+++ tests/psocketaddress_test.cpp
@@ -40,6 +40,7 @@ BOOST_AUTO_TEST_CASE (psocketaddress_bad_input_test)
 
 	BOOST_CHECK (p_socket_address_new_from_native (NULL, 0) == NULL);
 	BOOST_CHECK (p_socket_address_new (NULL, 0) == NULL);
+	BOOST_CHECK (p_socket_address_new ("bad_address", 0) == NULL);
 	BOOST_CHECK (p_socket_address_new_any (P_SOCKET_FAMILY_UNKNOWN, 0) == NULL);
 	BOOST_CHECK (p_socket_address_new_loopback (P_SOCKET_FAMILY_UNKNOWN, 0) == NULL);
 	BOOST_CHECK (p_socket_address_to_native (NULL, NULL, 0) == FALSE);
@@ -59,13 +60,13 @@ BOOST_AUTO_TEST_CASE (psocketaddress_general_test)
 {
 	p_lib_init ();
 
-	/* Test LAN address */
+	/* Test IPv4 LAN address */
 	PSocketAddress *addr = p_socket_address_new ("192.168.0.1", 2345);
 
 	BOOST_REQUIRE (addr != NULL);
 	BOOST_CHECK (p_socket_address_is_loopback (addr) == FALSE);
 	BOOST_CHECK (p_socket_address_is_any (addr) == FALSE);
-	BOOST_CHECK (p_socket_address_get_family (addr) != P_SOCKET_FAMILY_UNKNOWN);
+	BOOST_CHECK (p_socket_address_get_family (addr) == P_SOCKET_FAMILY_INET);
 	BOOST_CHECK (p_socket_address_get_port (addr) == 2345);
 	BOOST_CHECK (p_socket_address_get_native_size (addr) > 0);
 
@@ -77,7 +78,27 @@ BOOST_AUTO_TEST_CASE (psocketaddress_general_test)
 	p_free (addr_str);
 	p_socket_address_free (addr);
 
-	/* Test loopback address */
+#ifdef AF_INET6
+	/* Test IPv6 LAN address */
+	addr = p_socket_address_new ("2001:cdba:345f:24ab:fe45:5423:3257:9652", 2345);
+
+	BOOST_REQUIRE (addr != NULL);
+	BOOST_CHECK (p_socket_address_is_loopback (addr) == FALSE);
+	BOOST_CHECK (p_socket_address_is_any (addr) == FALSE);
+	BOOST_CHECK (p_socket_address_get_family (addr) == P_SOCKET_FAMILY_INET6);
+	BOOST_CHECK (p_socket_address_get_port (addr) == 2345);
+	BOOST_CHECK (p_socket_address_get_native_size (addr) > 0);
+
+	addr_str = p_socket_address_get_address (addr);
+
+	BOOST_REQUIRE (addr_str != NULL);
+	BOOST_CHECK (strcmp (addr_str, "2001:cdba:345f:24ab:fe45:5423:3257:9652") == 0);
+
+	p_free (addr_str);
+	p_socket_address_free (addr);
+#endif
+
+	/* Test IPv4 loopback address */
 	addr = p_socket_address_new_loopback (P_SOCKET_FAMILY_INET, 2345);
 
 	BOOST_REQUIRE (addr != NULL);
@@ -89,7 +110,21 @@ BOOST_AUTO_TEST_CASE (psocketaddress_general_test)
 
 	p_socket_address_free (addr);
 
-	/* Test any interface */
+#ifdef AF_INET6
+	/* Test IPv6 loopback address */
+	addr = p_socket_address_new_loopback (P_SOCKET_FAMILY_INET6, 2345);
+
+	BOOST_REQUIRE (addr != NULL);
+	BOOST_CHECK (p_socket_address_is_loopback (addr) == TRUE);
+	BOOST_CHECK (p_socket_address_is_any (addr) == FALSE);
+	BOOST_CHECK (p_socket_address_get_family (addr) == P_SOCKET_FAMILY_INET6);
+	BOOST_CHECK (p_socket_address_get_port (addr) == 2345);
+	BOOST_CHECK (p_socket_address_get_native_size (addr) > 0);
+
+	p_socket_address_free (addr);
+#endif
+
+	/* Test IPv4 any interface */
 	addr = p_socket_address_new_any (P_SOCKET_FAMILY_INET, 2345);
 
 	BOOST_REQUIRE (addr != NULL);
@@ -103,9 +138,10 @@ BOOST_AUTO_TEST_CASE (psocketaddress_general_test)
 
 	p_socket_address_free (addr);
 
-	/* Test native raw data */
+	/* Test IPv4 native raw data */
 	ppointer native_buf = p_malloc0 (native_size);
 	BOOST_CHECK (native_buf != NULL);
+	BOOST_CHECK (p_socket_address_new_from_native (native_buf, native_size) == NULL);
 	addr = p_socket_address_new ("192.168.0.2", 2345);
 	BOOST_REQUIRE (addr != NULL);
 	BOOST_CHECK (p_socket_address_to_native (addr, native_buf, native_size) == TRUE);
@@ -116,7 +152,7 @@ BOOST_AUTO_TEST_CASE (psocketaddress_general_test)
 	BOOST_CHECK (addr != NULL);
 	BOOST_CHECK (p_socket_address_is_loopback (addr) == FALSE);
 	BOOST_CHECK (p_socket_address_is_any (addr) == FALSE);
-	BOOST_CHECK (p_socket_address_get_family (addr) != P_SOCKET_FAMILY_UNKNOWN);
+	BOOST_CHECK (p_socket_address_get_family (addr) == P_SOCKET_FAMILY_INET);
 	BOOST_CHECK (p_socket_address_get_port (addr) == 2345);
 	BOOST_CHECK (p_socket_address_get_native_size (addr) == native_size);
 
@@ -125,8 +161,52 @@ BOOST_AUTO_TEST_CASE (psocketaddress_general_test)
 	BOOST_REQUIRE (addr_str != NULL);
 	BOOST_CHECK (strcmp (addr_str, "192.168.0.2") == 0);
 
+	p_free (native_buf);
 	p_free (addr_str);
 	p_socket_address_free (addr);
+
+#ifdef AF_INET6
+	/* Test IPv6 any interface */
+	addr = p_socket_address_new_any (P_SOCKET_FAMILY_INET6, 2345);
+
+	BOOST_REQUIRE (addr != NULL);
+	BOOST_CHECK (p_socket_address_is_loopback (addr) == FALSE);
+	BOOST_CHECK (p_socket_address_is_any (addr) == TRUE);
+	BOOST_CHECK (p_socket_address_get_family (addr) == P_SOCKET_FAMILY_INET6);
+	BOOST_CHECK (p_socket_address_get_port (addr) == 2345);
+	BOOST_CHECK (p_socket_address_get_native_size (addr) > 0);
+
+	native_size = p_socket_address_get_native_size (addr);
+
+	p_socket_address_free (addr);
+
+	/* Test IPv6 native raw data */
+	native_buf = p_malloc0 (native_size);
+	BOOST_CHECK (native_buf != NULL);
+	BOOST_CHECK (p_socket_address_new_from_native (native_buf, native_size) == NULL);
+	addr = p_socket_address_new ("2001:cdba:345f:24ab:fe45:5423:3257:9652", 2345);
+	BOOST_REQUIRE (addr != NULL);
+	BOOST_CHECK (p_socket_address_to_native (addr, native_buf, native_size) == TRUE);
+	p_socket_address_free (addr);
+
+	addr = p_socket_address_new_from_native (native_buf, native_size);
+
+	BOOST_CHECK (addr != NULL);
+	BOOST_CHECK (p_socket_address_is_loopback (addr) == FALSE);
+	BOOST_CHECK (p_socket_address_is_any (addr) == FALSE);
+	BOOST_CHECK (p_socket_address_get_family (addr) == P_SOCKET_FAMILY_INET6);
+	BOOST_CHECK (p_socket_address_get_port (addr) == 2345);
+	BOOST_CHECK (p_socket_address_get_native_size (addr) == native_size);
+
+	addr_str = p_socket_address_get_address (addr);
+
+	BOOST_REQUIRE (addr_str != NULL);
+	BOOST_CHECK (strcmp (addr_str, "2001:cdba:345f:24ab:fe45:5423:3257:9652") == 0);
+
+	p_free (native_buf);
+	p_free (addr_str);
+	p_socket_address_free (addr);
+#endif
 
 	p_lib_shutdown ();
 }
