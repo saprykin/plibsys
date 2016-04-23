28695c7eff45241c69b80efe7477e4a12aa5e26c
Alexander Saprykin <xelfium@gmail.com>
2016-04-24 00:36:04 +0300

    tests: Add out of memory checks for PError

--- tests/perror_test.cpp
+++ tests/perror_test.cpp
@@ -35,7 +35,55 @@
 #define PERROR_TEST_MESSAGE	"PError test error message"
 #define PERROR_TEST_MESSAGE_2	"Another PError test error message"
 
+extern "C" ppointer pmem_alloc (psize nbytes)
+{
+	P_UNUSED (nbytes);
+	return (ppointer) NULL;
+}
+
+extern "C" ppointer pmem_realloc (ppointer block, psize nbytes)
+{
+	P_UNUSED (block);
+	P_UNUSED (nbytes);
+	return (ppointer) NULL;
+}
+
+extern "C" void pmem_free (ppointer block)
+{
+	P_UNUSED (block);
+}
+
 BOOST_AUTO_TEST_SUITE (BOOST_TEST_MODULE)
+
+BOOST_AUTO_TEST_CASE (perror_nomem_test)
+{
+	p_lib_init ();
+
+	PError *error = p_error_new_literal (0, 0, NULL);
+	BOOST_CHECK (error != NULL);
+
+	PMemVTable vtable;
+
+	vtable.free	= pmem_free;
+	vtable.malloc	= pmem_alloc;
+	vtable.realloc	= pmem_realloc;
+
+	BOOST_CHECK (p_mem_set_vtable (&vtable) == TRUE);
+
+	BOOST_CHECK (p_error_new () == NULL);
+	BOOST_CHECK (p_error_new_literal (0, 0, NULL) == NULL);
+	BOOST_CHECK (p_error_copy (error) == NULL);
+
+	vtable.malloc	= (ppointer (*)(psize)) malloc;
+	vtable.realloc	= (ppointer (*)(ppointer, psize)) realloc;
+	vtable.free	= (void (*)(ppointer)) free;
+
+	BOOST_CHECK (p_mem_set_vtable (&vtable) == TRUE);
+
+	p_error_free (error);
+
+	p_lib_shutdown ();
+}
 
 BOOST_AUTO_TEST_CASE (perror_invalid_test)
 {
