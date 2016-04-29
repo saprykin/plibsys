6b77f1523a243033f697c5b4317a7be0da0e7b6e
Alexander Saprykin <xelfium@gmail.com>
2016-04-29 23:45:25 +0300

    docs: Rewrite documentation for the threads

--- src/puthread.h
+++ src/puthread.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2010-2016 Alexander Saprykin <xelfium@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -21,19 +21,32 @@
  * @brief Multithreading support
  * @author Alexander Saprykin
  *
- * If you need to suppoort multiple threads in the program, #PUThread gives
- * a convenient way to do it. #PUThread tries to use native system calls
- * wherenever it's possible: pthreads on UNIX, WinAPI threads on Windows,
- * LWP on Solaris. This guarantees the best performance you can achieve on
- * the target system.
- * To create thread use p_uthread_create() and p_uthread_create_full() methods.
- * You can also specify whether new thread should be joinable or not. Be careful
- * when specifying thread priority: very high priority can take all OS scheduler's
- * time so other threads will get almost zero run time and system may hang down.
- * Priorities may not work on all target systems, because not all of them take
- * thread priority into account (rather than for processes).
- * Also p_uthread_sleep() method can be used to sleep currently running thread for
- * given amount of time.
+ * Thread is a system execution unit which is managed independently by a
+ * scheduler of the operating system. It allows to do things in parallel or
+ * concurrently.
+ *
+ * #PUThread provides a convinient way of multithreading support using native
+ * routines to provide best performance on a target system.
+ *
+ * To create a thread use p_uthread_create() or p_uthread_create_full() routines.
+ * Joinable threads allow to wait until their execution is finished before
+ * proceeding further. Thus you can synchronize threads execution within the
+ * main thread.
+ *
+ * Priorities (if supported) allow to tune scheduler behavior: threads with
+ * higher priority will be executed more frequenlty. Be careful that improper
+ * priorities may lead to negative effects when some threads may receive almost
+ * zero execution time. Not all operating systems respect thread priorities in
+ * favour of process ones.
+ *
+ * To put a current thread (even if it was not created using #PUThread routines)
+ * in sleep state use p_uthread_sleep().
+ *
+ * You can give a hint to a scheduler that a current thread do not need an
+ * execution time with p_uthread_yield() routine. This is useful when some of
+ * the threads are in the idle state so you do not want to waste a CPU time.
+ * This only tells to a scheduler to skip current scheduling cycle for the
+ * calling thread, though the scheduler can ingnore it.
  */
 
 #if !defined (__PLIBSYS_H_INSIDE__) && !defined (PLIBSYS_COMPILATION)
@@ -58,16 +71,16 @@ typedef struct _PUThread PUThread;
 typedef enum _PUThreadPriority {
 	P_UTHREAD_PRIORITY_LOWEST	= 0,	/**< Lowest possible priority.					*/
 	P_UTHREAD_PRIORITY_LOW		= 1,	/**< Low priority (min_prio * 6 + normal_prio * 4) / 10.	*/
-	P_UTHREAD_PRIORITY_NORMAL	= 2,	/**< Normal priority (OS scheduler's priority).			*/
+	P_UTHREAD_PRIORITY_NORMAL	= 2,	/**< Normal priority (scheduler's priority).			*/
 	P_UTHREAD_PRIORITY_HIGH		= 3,	/**< High priority ((normal_prio + max_prio * 2) / 3).		*/
 	P_UTHREAD_PRIORITY_HIGHEST	= 4	/**< Highest possible priority.					*/
 } PUThreadPriority;
 
 /**
- * @brief Creates new #PUThread and starts it.
+ * @brief Creates a new #PUThread and starts it.
  * @param func Main thread function to run.
- * @param data Pointer to pass into thread main function, may be NULL.
- * @param joinable Whether create joinable thread or not.
+ * @param data Pointer to pass into the thread main function, may be NULL.
+ * @param joinable Whether to create joinable thread or not.
  * @param prio Thread priority.
  * @return Pointer to #PUThread in case of success, NULL otherwise.
  * @since 0.0.1
@@ -78,10 +91,10 @@ P_LIB_API PUThread *		p_uthread_create_full	(PUThreadFunc		func,
 							 PUThreadPriority	prio);
 
 /**
- * @brief Creates #PUThread and starts it. Short version of p_uthread_create_full().
+ * @brief Creates a #PUThread and starts it. Short version of p_uthread_create_full().
  * @param func Main thread function to run.
- * @param data Pointer to pass into thread main function, may be NULL.
- * @param joinable Whether create joinable thread or not.
+ * @param data Pointer to pass into the thread main function, may be NULL.
+ * @param joinable Whether to create joinable thread or not.
  * @return Pointer to #PUThread in case of success, NULL otherwise.
  * @since 0.0.1
  */
@@ -90,15 +103,15 @@ P_LIB_API PUThread *		p_uthread_create	(PUThreadFunc		func,
 							 pboolean		joinable);
 
 /**
- * @brief Exits from currently running (caller) thread.
+ * @brief Exits from the currently running (caller) thread.
  * @param code Exit code.
  * @since 0.0.1
  */
 P_LIB_API P_NO_RETURN void	p_uthread_exit		(pint			code);
 
 /**
- * @brief Waits for selected thread to become finished. Thread must be joinable to
- * return non-negative result.
+ * @brief Waits for selected thread to become finished. Thread must be joinable
+ * to return non-negative result.
  * @param thread Thread to wait for.
  * @return Thread exit code in case of success, -1 otherwise.
  * @since 0.0.1
@@ -108,13 +121,13 @@ P_LIB_API pint			p_uthread_join		(PUThread		*thread);
 /**
  * @brief Frees #PUThread and it's resources.
  * @param thread #PUThread to free.
- * @note Freeing running thread can lead to unpredictable behavior.
+ * @note Freeing the running thread can lead to unpredictable behavior.
  * @since 0.0.1
  */
 P_LIB_API void			p_uthread_free		(PUThread		*thread);
 
 /**
- * @brief Sleeps current thread (caller) for specified amount of time.
+ * @brief Sleeps the current thread (caller) for specified amount of time.
  * @param msec Milliseconds to sleep.
  * @return 0 in case of success, -1 otherwise.
  * @since 0.0.1
@@ -132,9 +145,9 @@ P_LIB_API pint			p_uthread_set_priority	(PUThread		*thread,
 							 PUThreadPriority	prio);
 
 /**
- * @brief Tells scheduler to skip current (caller) thread in current planning stage.
- * In other words, scheduler shouldn't give time ticks for the current thread during
- * current period.
+ * @brief Tells scheduler to skip the current (caller) thread in the current
+ * planning stage. In other words, scheduler shouldn't give time ticks for the
+ * current thread during the current period.
  * @since 0.0.1
  */
 P_LIB_API void			p_uthread_yield		(void);
