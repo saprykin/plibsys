1215de1ba65bc3bf085478c6d6fdf43947eb0c6a
Alexander Saprykin <xelfium@gmail.com>
2016-05-02 00:30:32 +0400

    tests: Add checks for TLS related API

--- tests/puthread_test.cpp
+++ tests/puthread_test.cpp
@@ -38,6 +38,9 @@ static volatile pboolean is_threads_working = TRUE;
 static P_HANDLE thread1_id = (P_HANDLE) NULL;
 static P_HANDLE thread2_id = (P_HANDLE) NULL;
 
+static PUThreadKey *tls_key = NULL;
+static volatile pint free_counter = 0;
+
 extern "C" ppointer pmem_alloc (psize nbytes)
 {
 	P_UNUSED (nbytes);
@@ -56,6 +59,12 @@ extern "C" void pmem_free (ppointer block)
 	P_UNUSED (block);
 }
 
+static void free_with_check (ppointer mem)
+{
+	p_free (mem);
+	p_atomic_int_inc (&free_counter);
+}
+
 static void * test_thread_func (void *data)
 {
 	pint *counter =  static_cast < pint * > (data);
@@ -65,12 +74,17 @@ static void * test_thread_func (void *data)
 	else
 		thread2_id = p_uthread_current_id ();
 
+	p_uthread_set_local (tls_key, (ppointer) p_uthread_current_id ());
+
 	*counter = 0;
 
 	while (is_threads_working == TRUE) {
 		p_uthread_sleep (10);
 		++(*counter);
 		p_uthread_yield ();
+
+		if (p_uthread_get_local (tls_key) != (ppointer) p_uthread_current_id ())
+			p_uthread_exit (-1);
 	}
 
 	p_uthread_exit (*counter);
@@ -93,11 +107,51 @@ static void * test_thread_nonjoinable_func (void *data)
 	p_uthread_exit (0);
 }
 
+static void * test_thread_tls_func (void *data)
+{
+	pint self_thread_free = *((pint *) data);
+
+	pint *tls_value = (pint *) p_malloc0 (sizeof (pint));
+	*tls_value = 0;
+	p_uthread_set_local (tls_key, (ppointer) tls_value);
+
+	pint prev_tls = 0;
+	pint counter  = 0;
+
+	while (is_threads_working == TRUE) {
+		p_uthread_sleep (10);
+
+		pint *last_tls = (pint *) p_uthread_get_local (tls_key);
+
+		if ((*last_tls) != prev_tls)
+			p_uthread_exit (-1);
+
+		pint *tls_new_value = (pint *) p_malloc0 (sizeof (pint));
+
+		*tls_new_value = (*last_tls) + 1;
+		prev_tls       = (*last_tls) + 1;
+
+		p_uthread_replace_local (tls_key, (ppointer) tls_new_value);
+
+		if (self_thread_free)
+			p_free (last_tls);
+
+		++counter;
+
+		p_uthread_yield ();
+	}
+
+	p_uthread_exit (counter);
+}
+
 BOOST_AUTO_TEST_SUITE (BOOST_TEST_MODULE)
 
 BOOST_AUTO_TEST_CASE (puthread_nomem_test)
 {
 	p_libsys_init ();
+
+	PUThreadKey *thread_key = p_uthread_local_new (NULL);
+	BOOST_CHECK (thread_key != NULL);
 
 	PMemVTable vtable;
 
@@ -116,11 +170,18 @@ BOOST_AUTO_TEST_CASE (puthread_nomem_test)
 					    TRUE,
 					    P_UTHREAD_PRIORITY_NORMAL) == NULL);
 
+	BOOST_CHECK (p_uthread_local_new (NULL) == NULL);
+
+	p_uthread_set_local (thread_key, PINT_TO_POINTER (10));
+	BOOST_CHECK (p_uthread_get_local (thread_key) == NULL);
+
 	vtable.malloc	= (ppointer (*)(psize)) malloc;
 	vtable.realloc	= (ppointer (*)(ppointer, psize)) realloc;
 	vtable.free	= (void (*)(ppointer)) free;
 
 	BOOST_CHECK (p_mem_set_vtable (&vtable) == TRUE);
+
+	p_uthread_local_free (thread_key);
 
 	p_libsys_shutdown ();
 }
@@ -133,7 +194,11 @@ BOOST_AUTO_TEST_CASE (puthread_bad_input_test)
 	BOOST_CHECK (p_uthread_create_full (NULL, NULL, false, P_UTHREAD_PRIORITY_NORMAL) == NULL);
 	BOOST_CHECK (p_uthread_join (NULL) == -1);
 	BOOST_CHECK (p_uthread_set_priority (NULL, P_UTHREAD_PRIORITY_NORMAL));
+	BOOST_CHECK (p_uthread_get_local (NULL) == NULL);
+	p_uthread_set_local (NULL, NULL);
+	p_uthread_replace_local (NULL, NULL);
 	p_uthread_free (NULL);
+	p_uthread_local_free (NULL);
 
 	p_libsys_shutdown ();
 }
@@ -144,6 +209,9 @@ BOOST_AUTO_TEST_CASE (puthread_general_test)
 
 	thread_wakes_1 = 1;
 	thread_wakes_2 = 2;
+
+	tls_key = p_uthread_local_new (NULL);
+	BOOST_CHECK (tls_key != NULL);
 
 	PUThread *thr1 = p_uthread_create ((PUThreadFunc) test_thread_func,
 					   (ppointer) &thread_wakes_1,
@@ -167,6 +235,10 @@ BOOST_AUTO_TEST_CASE (puthread_general_test)
 	BOOST_CHECK (p_uthread_join (thr2) == thread_wakes_2);
 
 	BOOST_REQUIRE (thread1_id != thread2_id);
+	BOOST_CHECK (thread1_id != p_uthread_current_id () &&
+		     thread2_id != p_uthread_current_id ());
+
+	p_uthread_local_free (tls_key);
 
 	p_libsys_shutdown ();
 }
@@ -191,6 +263,76 @@ BOOST_AUTO_TEST_CASE (puthread_nonjoinable_test)
 		p_uthread_sleep (10);
 
 	BOOST_CHECK (thread_wakes_1 == thread_to_wakes);
+
+	p_libsys_shutdown ();
+}
+
+BOOST_AUTO_TEST_CASE (puthread_tls_test)
+{
+	p_libsys_init ();
+
+	/* With destroy notification */
+	tls_key = p_uthread_local_new (free_with_check);
+
+	is_threads_working = TRUE;
+
+	pint self_thread_free = 0;
+
+	PUThread *thr1 = p_uthread_create ((PUThreadFunc) test_thread_tls_func,
+					   (ppointer) &self_thread_free,
+					   TRUE);
+
+	PUThread *thr2 = p_uthread_create ((PUThreadFunc) test_thread_tls_func,
+					   (ppointer) &self_thread_free,
+					   TRUE);
+
+	BOOST_REQUIRE (thr1 != NULL);
+	BOOST_REQUIRE (thr2 != NULL);
+
+	p_uthread_sleep (5000);
+
+	is_threads_working = FALSE;
+
+	pint total_counter = 0;
+
+	total_counter += (p_uthread_join (thr1) + 1);
+	total_counter += (p_uthread_join (thr2) + 1);
+
+	BOOST_CHECK (total_counter == free_counter);
+
+	p_uthread_local_free (tls_key);
+
+	/* Without destroy notification */
+	tls_key = p_uthread_local_new (NULL);
+
+	free_counter       = 0;
+	is_threads_working = TRUE;
+	self_thread_free   = 1;
+
+	thr1 = p_uthread_create ((PUThreadFunc) test_thread_tls_func,
+				 (ppointer) &self_thread_free,
+				 TRUE);
+
+	thr2 = p_uthread_create ((PUThreadFunc) test_thread_tls_func,
+				 (ppointer) &self_thread_free,
+				 TRUE);
+
+	BOOST_REQUIRE (thr1 != NULL);
+	BOOST_REQUIRE (thr2 != NULL);
+
+	p_uthread_sleep (5000);
+
+	is_threads_working = FALSE;
+
+	total_counter = 0;
+
+	total_counter += (p_uthread_join (thr1) + 1);
+	total_counter += (p_uthread_join (thr2) + 1);
+
+	BOOST_CHECK (total_counter > 0);
+	BOOST_CHECK (free_counter == 0);
+
+	p_uthread_local_free (tls_key);
 
 	p_libsys_shutdown ();
 }
