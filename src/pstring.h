fc8c258716a5a45d217ace12908061aac07febf0
Alexander Saprykin <xelfium@gmail.com>
2016-05-11 00:09:04 +0300

    docs: Improve documentation for strings

--- src/pstring.h
+++ src/pstring.h
@@ -17,10 +17,13 @@
 
 /**
  * @file pstring.h
- * @brief Strings manipulation routines
+ * @brief String manipulation routines
  * @author Alexander Saprykin
  *
- * Here are routines to manipulate with strings consist of chars and trailing \0.
+ * Strings are represented as a sequence of single-byte characters (from the
+ * ASCII table) with the trailing zero character (\0).
+ *
+ * Some useful string manipulation routines are represented here.
  */
 
 #if !defined (__PLIBSYS_H_INSIDE__) && !defined (PLIBSYS_COMPILATION)
@@ -36,34 +39,34 @@
 P_BEGIN_DECLS
 
 /**
- * @brief Copies string.
- * @param str String with trailing \0 to copy.
+ * @brief Copies a string.
+ * @param str String with the trailing zero to copy.
  * @return Copy of the @a str in case of success, NULL otherwise. Caller takes
- * ownership for the returned string.
+ * ownership of the returned string.
  * @since 0.0.1
  */
-P_LIB_API pchar *	p_strdup	(const pchar *str);
+P_LIB_API pchar *	p_strdup	(const pchar	*str);
 
 /**
  * @brief Removes trailing and leading whitespaces.
- * @param str String with trailing \0 to process.
- * @return Newlly allocated string in case of success, NULL otherwise. Caller takes
- * ownership for the returned string.
+ * @param str String with the trailing zero to process.
+ * @return Newlly allocated string in case of success, NULL otherwise. Caller
+ * takes ownership of the returned string.
  * @since 0.0.1
  */
-P_LIB_API pchar *	p_strchomp	(const pchar *str);
+P_LIB_API pchar *	p_strchomp	(const pchar	*str);
 
 /**
- * @brief Tokenizes string by given delimiters.
+ * @brief Tokenizes a string by given delimiters.
  * @param[in, out] str String to tokenize.
  * @param delim List of delimiters to split the string.
  * @param buf Context to store tokenize info.
- * @return Pointer to splitted zero-terminated string in case of success, NULL
+ * @return Pointer to a splitted zero-terminated string in case of success, NULL
  * otherwise.
  * @since 0.0.1
- * @note @a str is modified by this call, so take care for that.
- * Returned pointer points on the @a str substring, so you do not need
- * to call p_free() on it.
+ * @note @a str is modified by this call, so take care for that. The returned
+ * pointer points on the @a str substring, so you do not need to call p_free()
+ * on it.
  *
  * The common usage of this call is following:
  * @code
@@ -78,26 +81,26 @@ P_LIB_API pchar *	p_strchomp	(const pchar *str);
  *     token = p_strtok (NULL, delim, &buf);
  * }
  * @endcode
- * Take an attention that you need to pass original string only
- * once, then you should pass NULL instead. You can also pass
- * different delimiters each time.
+ * Take an attention that you need to pass the original string only once, after
+ * that you should pass NULL instead. You can also pass different delimiters
+ * each time.
  *
- * Some platforms do not support third parameter and it can be
- * remained unused. In that case this call wouldn't be thread-safe.
+ * Some platforms do not support third parameter and it can be remained unused.
+ * In that case this call wouldn't be thread-safe.
  */
 P_LIB_API pchar *	p_strtok	(pchar		*str,
 					 const pchar	*delim,
 					 pchar		**buf);
 
 /**
- * @brief Converts string to @a double without locale dependency
+ * @brief Converts a string to @a double without a locale dependency.
  * @param str String to convert.
  * @return Floating point value in case of success, 0 otherwise.
  * @since 0.0.1
  *
- * Since atof() system call is locale dependent, you can use this
- * call to convert string variables to @a double values. The
- * decimal point is '.' as in 'C' locale.
+ * Since the atof() system call is locale dependent, you can use this call to
+ * convert string variables to @a double values. The decimal point is '.' as i
+ * the 'C' locale.
  */
 P_LIB_API double	p_strtod	(const pchar	*str);
 
