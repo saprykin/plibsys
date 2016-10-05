0159eb52f84466781b96db091f5fc4669918508d
Alexander Saprykin <xelfium@gmail.com>
2016-10-05 21:41:42 +0300

    cmake: Fix Clang compiler warnings under Windows

--- platforms/win32-clang/platform.cmake
+++ platforms/win32-clang/platform.cmake
@@ -3,4 +3,5 @@ set (PLIBSYS_IPC_MODEL win)
 set (PLIBSYS_TIME_PROFILER_MODEL win)
 set (PLIBSYS_DIR_MODEL win)
 
+set (PLIBSYS_PLATFORM_CFLAGS "-Wno-missing-braces -Wmissing-field-initializers")
 set (PLIBSYS_PLATFORM_LINK_LIBRARIES ws2_32)
