8f058a5c7eb59b44a10d3214ac3dc2fd52486444
Alexander Saprykin <xelfium@gmail.com>
2017-06-04 10:57:48 +0200

    cmake: Add automatic detection of visibility support in compiler

---
+++ cmake/VisibilityDetect.cmake
@@ -0,0 +1,95 @@
+function (plibsys_detect_visibility cflags ldflags)
+        # Check GCC
+        check_c_source_compiles ("int main () {
+                                        #if (__GNUC__ >= 4) && \
+                                            !defined(__sun)  && !defined(sun)  && \
+                                            !defined(__hpux) && !defined(hpux) && \
+                                            !defined(_AIX) && !defined(__CYGWIN__) && !defined(__MSYS__)
+                                        return 0;
+                                        #else
+                                        #  error \"No visibility\"
+                                        #endif
+                                 }"
+                                 PLIBSYS_HAS_GCC_VISIBILITY
+        )
+
+        if (PLIBSYS_HAS_GCC_VISIBILITY)
+                set (${cflags} "-fvisibility=hidden" PARENT_SCOPE)
+                set (${ldflags} "" PARENT_SCOPE)
+                return()
+        endif()
+
+        # Check Sun Studio
+        check_c_source_compiles ("int main () {
+                                        #if (defined(__SUNPRO_C) && (__SUNPRO_C >= 0x550)) || \
+                                            (defined(__SUNPRO_CC) && (__SUNPRO_CC >= 0x5110))
+                                        return 0;
+                                        #else
+                                        #  error \"No visibility\"
+                                        #endif
+                                 }"
+                                 PLIBSYS_HAS_SUN_VISIBILITY
+        )
+
+        if (PLIBSYS_HAS_SUN_VISIBILITY)
+                set (${cflags} "" PARENT_SCOPE)
+                set (${ldlags} "-xldscope=__hidden" PARENT_SCOPE)
+                return()
+        endif()
+
+        # Check IBM XL C
+        check_c_source_compiles ("int main () {
+                                        #if (defined(__xlC__) && (__xlC__ >= 0x0D01))
+                                        return 0;
+                                        #else
+                                        #  error \"No visibility\"
+                                        #endif
+                                 }"
+                                 PLIBSYS_HAS_XLC_VISIBILITY
+        )
+
+        if (PLIBSYS_HAS_XLC_VISIBILITY)
+                set (${cflags} "-qvisibility=hidden" PARENT_SCOPE)
+                set (${ldflags} "" PARENT_SCOPE)
+                return()
+        endif()
+
+        # Check HP C/aC++
+        check_c_source_compiles ("int main () {
+                                        #if (defined(__HP_cc)  && (__HP_cc >= 0x061500)) || \
+                                            (defined(__HP_aCC) && (__HP_aCC >= 0x061500))
+                                        return 0;
+                                        #else
+                                        #  error \"No visibility\"
+                                        #endif
+                                 }"
+                                 PLIBSYS_HAS_HP_VISIBILITY
+        )
+
+        if (PLIBSYS_HAS_HP_VISIBILITY)
+                set (${cflags} "-Bhidden" PARENT_SCOPE)
+                set (${ldflags} "" PARENT_SCOPE)
+                return()
+        endif()
+
+        # Check Clang
+        check_c_source_compiles ("int main () {
+                                        #if defined(__has_attribute) && __has_attribute(visibility)
+                                        return 0;
+                                        #else
+                                        #  error \"No visibility\"
+                                        #endif
+                                 }"
+                                 PLIBSYS_HAS_CLANG_VISIBILITY
+        )
+
+        if (PLIBSYS_HAS_CLANG_VISIBILITY)
+                set (${cflags} "-fvisibility=hidden" PARENT_SCOPE)
+                set (${ldflags} "" PARENT_SCOPE)
+                return()
+        endif()
+
+        # Empty result
+        set (${cflags} "" PARENT_SCOPE)
+        set (${ldflags} "" PARENT_SCOPE)
+endfunction (plibsys_detect_visibility)
