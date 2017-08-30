e200837e7dc6deba53fae43eb924a4269934af48
Alexander Saprykin <xelfium@gmail.com>
2017-08-30 18:07:13 +0200

    tests: Move file functions on new testing macros

--- tests/pfile_test.cpp
+++ tests/pfile_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2016 Alexander Saprykin <xelfium@gmail.com>
+ * Copyright (C) 2013-2017 Alexander Saprykin <xelfium@gmail.com>
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,45 +15,39 @@
  * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef PLIBSYS_TESTS_STATIC
-#  define BOOST_TEST_DYN_LINK
-#endif
-
-#define BOOST_TEST_MODULE pfile_test
-
 #include "plibsys.h"
+#include "ptestmacros.h"
 
 #include <stdio.h>
 
-#ifdef PLIBSYS_TESTS_STATIC
-#  include <boost/test/included/unit_test.hpp>
-#else
-#  include <boost/test/unit_test.hpp>
-#endif
+P_TEST_MODULE_INIT ();
 
 #define PFILE_TEST_FILE "." P_DIR_SEPARATOR "pfile_test_file.txt"
 
-BOOST_AUTO_TEST_SUITE (BOOST_TEST_MODULE)
-
-BOOST_AUTO_TEST_CASE (pfile_general_test)
+P_TEST_CASE_BEGIN (pfile_general_test)
 {
 	p_libsys_init ();
 
-	BOOST_CHECK (p_file_remove (NULL, NULL) == FALSE);
+	P_TEST_CHECK (p_file_remove (NULL, NULL) == FALSE);
 
-	BOOST_CHECK (p_file_is_exists (PFILE_TEST_FILE) == FALSE);
-	BOOST_CHECK (p_file_remove ("." P_DIR_SEPARATOR" pfile_test_file_remove.txt", NULL) == FALSE);
+	P_TEST_CHECK (p_file_is_exists (PFILE_TEST_FILE) == FALSE);
+	P_TEST_CHECK (p_file_remove ("." P_DIR_SEPARATOR" pfile_test_file_remove.txt", NULL) == FALSE);
 
 	FILE *file = fopen (PFILE_TEST_FILE, "w");
-	BOOST_REQUIRE (file != NULL);
-	BOOST_CHECK (p_file_is_exists (PFILE_TEST_FILE) == TRUE);
+	P_TEST_REQUIRE (file != NULL);
+	P_TEST_CHECK (p_file_is_exists (PFILE_TEST_FILE) == TRUE);
 
 	fprintf (file, "This is a test file string\n");
 
-	BOOST_CHECK (fclose (file) == 0);
-	BOOST_CHECK (p_file_remove (PFILE_TEST_FILE, NULL) == TRUE);
+	P_TEST_CHECK (fclose (file) == 0);
+	P_TEST_CHECK (p_file_remove (PFILE_TEST_FILE, NULL) == TRUE);
 
 	p_libsys_shutdown ();
 }
+P_TEST_CASE_END ()
 
-BOOST_AUTO_TEST_SUITE_END()
+P_TEST_SUITE_BEGIN()
+{
+	P_TEST_SUITE_RUN_CASE (pfile_general_test);
+}
+P_TEST_SUITE_END()
