c1e2e16d598fda251eceda7970f7123938bfb4c7
Alexander Saprykin <xelfium@gmail.com>
2017-12-20 10:22:35 +0100

    cmake: Adjust coding style for stdarg detection

--- cmake/StdargDetect.cmake
+++ cmake/StdargDetect.cmake
@@ -4,22 +4,21 @@ function (plibsys_detect_va_copy result)
 	# there is sometimes the need to do va_copy() when
 	# calling another va_list aware function.
 	#
-	# This mean that va_copy() is not needed (thus not available)
+	# This means that va_copy() is not needed (thus not available)
 	# everywhere.
 	#
-	# We depend on stdarg in any case, stdio in some cases.
+	# We depend on stdarg.h in any case, stdio.h in some cases.
 	#
-	# Know implementations vary from va_copy to __va_copy (old proposed name).
+	# Known implementations vary from va_copy to __va_copy (old proposed name).
 	# We check the _va_copy eventually.
 	#
 	set (P_VA_COPY FALSE)
 	foreach (KEYWORD "va_copy" "_va_copy" "__va_copy")
-		check_c_source_compiles (
-					"
-					#include <stdio.h>
-					#include <stdarg.h>
+		check_c_source_compiles ("
+					 #include <stdio.h>
+					 #include <stdarg.h>
 
-					void f (int i, ...) {
+					 void f (int i, ...) {
 						va_list args1, args2;
 
 						va_start (args1, i);
@@ -29,19 +28,20 @@ function (plibsys_detect_va_copy result)
 						}
 						va_end (args1);
 						va_end (args2);
-					}
+					 }
 
-					int main() {
+					 int main() {
 						f (0, 42);
 						exit(0);
-					}"
+					 }"
 					PLIBSYS_${KEYWORD}
 		)
+
 		if (PLIBSYS_${KEYWORD})
 			set (P_VA_COPY ${KEYWORD})
-			break ()
-		endif ()
-	endforeach ()
+			break()
+		endif()
+	endforeach()
 
 	# Assign result
 	set (${result} ${P_VA_COPY} PARENT_SCOPE)
