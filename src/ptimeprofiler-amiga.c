3c33bf0f396cbd2dba6685b69f1096cfc00a12aa
Alexander Saprykin <saprykin.spb@gmail.com>
2023-09-06 15:28:21 +0200

    amigaos: Do not rely on global library interfaces for ELF and EClock

--- src/ptimeprofiler-amiga.c
+++ src/ptimeprofiler-amiga.c
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (C) 2017 Alexander Saprykin <saprykin.spb@gmail.com>
+ * Copyright (C) 2017-2023 Alexander Saprykin <saprykin.spb@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -26,16 +26,50 @@
 #include "ptimeprofiler.h"
 #include "ptimeprofiler-private.h"
 
-#include <proto/timer.h>
+#include <exec/types.h>
+#include <exec/memory.h>
+#include <devices/timer.h>
+#include <interfaces/timer.h>
 
-static puint64 pp_time_profiler_freq = 1;
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+static struct MsgPort		*pp_port              = NULL;
+static struct TimeRequest	*pp_timer_io          = NULL;
+static struct TimerIFace	*pp_ITimer            = NULL;
+static puint64 			pp_time_profiler_freq = 1;
+
+static void
+pp_time_profiler_close_device (void)
+{
+	if (P_LIKELY (pp_ITimer != NULL)) {
+		IExec->DropInterface ((struct Interface *) pp_ITimer);
+		pp_ITimer = NULL;
+	}
+
+	if (P_LIKELY (pp_timer_io != NULL)) {
+		IExec->CloseDevice ((struct IORequest *) pp_timer_io);
+		IExec->FreeSysObject (ASOT_IOREQUEST, pp_timer_io);
+		IExec->FreeSysObject (ASOT_PORT, pp_port);
+
+		pp_timer_io = NULL;
+		pp_port     = NULL;
+	}
+
+	pp_time_profiler_freq = 1;
+}
 
 puint64
 p_time_profiler_get_ticks_internal ()
 {
 	struct EClockVal eclock;
 
-	ITimer->ReadEClock (&eclock);
+	if (P_UNLIKELY (pp_ITimer == NULL)) {
+		P_ERROR ("PTimeProfiler::p_time_profiler_get_ticks_internal: timer subsystem is not initialized");
+		return 0;
+	}
+
+	pp_ITimer->ReadEClock (&eclock);
 
 	return (((puint64) eclock.ev_hi) * pp_time_profiler_freq + (puint64) eclock.ev_lo);
 }
@@ -60,11 +94,32 @@ p_time_profiler_init (void)
 {
 	struct EClockVal eclock;
 
-	pp_time_profiler_freq = (puint64) ITimer->ReadEClock (&eclock);
+	pp_port = IExec->AllocSysObjectTags (ASOT_PORT, TAG_END);
+	pp_timer_io = IExec->AllocSysObjectTags (ASOT_IOREQUEST,
+						 ASOIOR_Size, sizeof (struct TimeRequest),
+						 ASOIOR_ReplyPort, pp_port,
+						 TAG_END);
+
+	if (P_UNLIKELY (IExec->OpenDevice (TIMERNAME, UNIT_ECLOCK, (struct IORequest *) pp_timer_io, 0L) != 0)) {
+		P_ERROR ("PTimeProfiler::p_time_profiler_init: failed to open timer device");
+		IExec->FreeSysObject (ASOT_IOREQUEST, pp_timer_io);
+		IExec->FreeSysObject (ASOT_PORT, pp_port);
+		return;
+	}
+
+	pp_ITimer = (struct TimerIFace *) IExec->GetInterface ((struct Library *) pp_timer_io->Request.io_Device, "main", 1, NULL);
+
+	if (P_UNLIKELY (pp_ITimer == NULL)) {
+		P_ERROR ("PTimeProfiler::p_time_profiler_init: failed to get timer interface");
+		pp_time_profiler_close_device ();	
+		return;
+	}
+
+	pp_time_profiler_freq = (puint64) pp_ITimer->ReadEClock (&eclock);
 }
 
 void
 p_time_profiler_shutdown (void)
 {
-	pp_time_profiler_freq = 1;
+	pp_time_profiler_close_device ();
 }
